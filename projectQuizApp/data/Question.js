adb4b51d766d8519714903be566aa6dbc72631b8
zeycoder <zynparsln.5561@gmail.com>
2022-08-01 17:35:17 +0300

    Create Questions.js

---
+++ projectQuizApp/data/Question.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        id:1,
+        soru:"Nerede yaşıyoruz ? ", 
+        secenek: ['Jupiter', 'Saturn' , 'World' , 'Mars'],
+        cevap:"Satürn",
+        a:"Jüpiter",
+        b:"Satürn",
+        c:"Dünya",
+        d:"Mars"
+    }
+]
