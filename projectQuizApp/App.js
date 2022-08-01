683ed5bbfdfd2b4adba6ccfb89265c81003da05b
zeycoder <zynparsln.5561@gmail.com>
2022-08-01 17:35:43 +0300

    Update App.js

--- projectQuizApp/App.js
+++ projectQuizApp/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { styles } from './style/Css';
+import { data } from './data/Question';
+
+const allQuestions = data;
+const [questionIndex, setQuestionIndex] = useState(0)
+
+
 
 export default function App() {
+  const renderOptions = () => {
+    return (
+      <View>
+        {allQuestions[questionIndex]?.secenek.map(secenek => (
+          <TouchableOpacity>
+            <Text> {secenek} </Text>
+          </TouchableOpacity>
+        )) }
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container} >
+      {data.map(list => {
+        return(
+          <View>
+            <Text style={styles.question} >{list.soru} </Text>
+            <Text> {list.secenek} </Text>
+          </View>
+        )
+      })}
+      {renderOptions}
+      <Text > {} </Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
