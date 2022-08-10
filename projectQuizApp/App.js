b3ac83a740470d5c4edd3a1d237fe414c0195e6c
zeycoder <zynparsln.5561@gmail.com>
2022-08-10 12:03:14 +0300

    Update App.js

--- projectQuizApp/App.js
+++ projectQuizApp/App.js
@@ -1,11 +1,54 @@
-import React from "react";
-import { Text, SafeAreaView} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, SafeAreaView, TextInput, View} from "react-native";
 import { styles } from "./style/Css";
 
 const App = () =>{
+
+  const [country, setCountry] = useState([])
+  const [text, setText] = useState('')
+
+  useEffect(()=>{
+
+    async function getCountry() {
+      const response =await fetch('https://restcountries.com/v3.1/all',{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          text
+        })
+      })
+    }
+
+    fetch('https://restcountries.com/v3.1/all')
+    
+  },[])
+    
+  var data = null;
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("GET", "https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=ankara");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("authorization", "apikey your_token");
+
+  xhr.send(data);
+
+
   return(
     <SafeAreaView style={styles.container} >
-      <Text>Selam</Text>
+      <Text>{data}</Text>
+      <View>
+
+      </View>
     </SafeAreaView>
   )
 }
